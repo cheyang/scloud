a372daae145c769162c907b93fd38e3d09becdb9
cheyang <cheyang@163.com>
2016-02-18 19:04:35 +0800

    add hostname helper

--- pkg/util/hostname_helper.go
+++ pkg/util/hostname_helper.go
@@ -8,17 +8,17 @@ import (
 // Support hostname extention from 001-fff(4095)
 
 const (
-	MaxValueForExtention = 4096
+	MaxValueForExtention int64 = 4096
 )
 
 func GetCurrentMaxExt(nums []string) string {
 
 	maxValue := 0
 
-	vMap := make(map[int]string, 0)
+	vMap := make(map[int64]string, 0)
 
 	for _, num := range nums {
-		key, err := int(strconv.ParseInt(num, 16, 0))
+		key, err := strconv.ParseInt(num, 16, 0)
 
 		if err != nil {
 			fmt.Println(err, "in GetCurrentMaxExt")
