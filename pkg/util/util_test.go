1eeac2232a55deef186465720b125f09df53435e
cheyang <cheyang@163.com>
2016-02-19 15:34:37 +0800

    add test

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -7,6 +7,20 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Util", func() {
+var _ = Describe("Hostname Helper", func() {
+	var (
+		nums []string
+	)
+
+	BeforeEach(func() {
+		nums = []string{"001", "002", "010", "00f", "xxy"}
+	})
+
+	Context("#GetCurrentMaxExt", func() {
+		It("Get the Max Value", func() {
+			value := GetCurrentMaxExt(nums)
+			Expect(value).To(Equal("010"))
+		})
+	})
 
 })
