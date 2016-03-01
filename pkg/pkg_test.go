f91bc20638fb9effe18dc12c8195ec9461a29bf0
cheyang <cheyang@163.com>
2016-03-01 14:09:32 +0800

    add testcase

--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -1,12 +1,26 @@
 package pkg_test
 
 import (
-	. "github.com/cheyang/scloud/pkg"
+	sl_cloud "github.com/cheyang/scloud/drivers/softlayer"
+	lib "github.com/cheyang/scloud/pkg"
+	"github.com/cheyang/scloud/pkg/persist"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Pkg", func() {
+var _ = Describe("pkg", func() {
 
+	Context("#Create", func() {
+
+		It("create a new VM on Softlayer", func() {
+			store := lib.GetDefaultStore("mytest")
+
+			hostname := "mytesthost"
+
+			sl_driver := sl_cloud.NewDriver(hostname, store.Path)
+
+			sl_driver.PreCreateCheck()
+		})
+	})
 })
