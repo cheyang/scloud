8ebccb525075e0bc78f675de1901128a0ae756cb
cheyang <cheyang@163.com>
2016-03-03 15:07:56 +0800

    runtime

--- pkg/msg/msg_suite_test.go
+++ pkg/msg/msg_suite_test.go
@@ -1,7 +1,7 @@
 package msg_test
 
 import (
-	//	scloudLog "github.com/cheyang/scloud/pkg/log"
+	scloudLog "github.com/cheyang/scloud/pkg/log"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -9,6 +9,14 @@ import (
 )
 
 func TestMsg(t *testing.T) {
+
+	err := scloudLog.InitLog()
+
+	if err != nil {
+		t.Errorf("Failed to init log")
+	}
+
+	defer scloudLog.CloseLog()
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Msg Suite")
