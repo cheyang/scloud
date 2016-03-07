a7cdf2476f7a82b51444ab1a8ca7edd7bb4d3359
cheyang <cheyang@163.com>
2016-03-07 17:54:53 +0800

    fix test

---
+++ pkg/deploy/deployment_test.go
@@ -0,0 +1,49 @@
+package deploy_test
+
+import (
+	. "github.com/cheyang/scloud/pkg/deploy"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Deploy", func() {
+
+	var (
+		spec    DeploymentSpec
+		roles   []*DeploymentRole
+		h       []*host.Host
+		specNum int
+	)
+
+	BeforeEach(func() {
+
+		roles = []*DeploymentRole{
+			DeploymentRole{
+				Name:           "kube-master",
+				MaxNum:         1,
+				MinNum:         1,
+				groupName:      "k8s1",
+				hostnamePrefix: "kubemaster",
+			},
+			DeploymentRole{
+				Name:   "kube-nodes",
+				MaxNum: 2,
+			},
+			DeploymentRole{
+				Name:      "etcd",
+				MaxNum:    3,
+				groupName: "k8s1",
+			},
+			DeploymentRole{
+				Name:        "registry",
+				IpAddresses: []string{"10.62.71.77"},
+			},
+		}
+
+	})
+
+	Context("#Generate deployment spec", func() {
+		Expect(roles[3].Name).To(Equal(registry))
+	})
+})
