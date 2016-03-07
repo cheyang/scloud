13ccefff4317797c531541c1ba987f9d92592ef6
cheyang <cheyang@163.com>
2016-03-07 16:19:00 +0800

    test deployment spec

--- pkg/deploy/deploy_test.go
+++ pkg/deploy/deploy_test.go
@@ -9,4 +9,41 @@ import (
 
 var _ = Describe("Deploy", func() {
 
+	var (
+		spec    deploy.DeploymentSpec
+		roles   []*deploy.DeploymentRole
+		h       []*host.Host
+		specNum int
+	)
+
+	BeforeEach(func() {
+
+		roles = []*deploy.DeploymentRole{
+			DeploymentRole{
+				Name:           "kube-master",
+				MaxNum:         1,
+				MinNum:         1,
+				groupName:      "k8s1",
+				hostnamePrefix: "kubemaster",
+			},
+			DeploymentRole{
+				Name:   "kube-nodes",
+				MaxNum: 2,
+			},
+			DeploymentRole{
+				Name:      "etcd",
+				MaxNum:    3,
+				groupName: "k8s1",
+			},
+			DeploymentRole{
+				Name:        "registry",
+				IpAddresses: []string{"10.62.71.77"},
+			},
+		}
+
+	})
+
+	Context("#Generate deployment spec", func() {
+		Expect(roles[3].Name).To(Equal(registry))
+	})
 })
