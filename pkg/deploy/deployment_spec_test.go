ed2875c70e783c43d76e90c73484943cf92ffea0
cheyang <cheyang@163.com>
2016-03-07 18:03:23 +0800

    fix test

---
+++ pkg/deploy/deployment_spec_test.go
@@ -0,0 +1,52 @@
+package deploy
+
+import (
+	//	. "github.com/cheyang/scloud/pkg/deploy"
+	//	"github.com/cheyang/scloud/pkg/host"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Deploy", func() {
+
+	var (
+		//		spec    DeploymentSpec
+		roles []*DeploymentRole
+		//		h       []*host.Host
+
+	)
+
+	BeforeEach(func() {
+
+		roles = []*DeploymentRole{
+			&DeploymentRole{
+				Name:           "kube-master",
+				MaxNum:         1,
+				MinNum:         1,
+				groupName:      "k8s1",
+				HostnamePrefix: "kubemaster",
+			},
+			&DeploymentRole{
+				Name:   "kube-nodes",
+				MaxNum: 2,
+			},
+			&DeploymentRole{
+				Name:      "etcd",
+				MaxNum:    3,
+				groupName: "k8s1",
+			},
+			&DeploymentRole{
+				Name:        "registry",
+				IpAddresses: []string{"10.62.71.77"},
+			},
+		}
+
+	})
+
+	Context("#Generate deployment spec", func() {
+		It("create a new VM on Softlayer", func() {
+			Expect(roles[3].Name).To(Equal("registry"))
+		})
+	})
+})
