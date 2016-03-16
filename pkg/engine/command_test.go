dd7c160c96f9fb78e4ea746d7447e7af99cfb34d
cheyang <cheyang@163.com>
2016-03-16 16:40:15 +0800

    test

--- pkg/engine/command_test.go
+++ pkg/engine/command_test.go
@@ -52,6 +52,41 @@ var _ = Describe("Test command", func() {
 
 		})
 
+		It("error to log file", func() {
+			cmd := NewCommand("grep")
+			cmd.SetWorkingDir("/tmp")
+
+			currentTime := time.Now().Unix()
+
+			tm := time.Unix(currentTime, 0)
+
+			timestamp := tm.Format("20060102150405")
+
+			logBaseName := fmt.Sprintf("test_%s.log", timestamp)
+
+			logFileName := filepath.Join("/tmp", logBaseName)
+
+			logFile, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0664)
+
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+
+			logFile.WriteString(fmt.Sprintf("%v Opened logfile at %v", os.Getpid(), time.Now()))
+
+			cmd.SetStdout(logFile)
+
+			cmd.SetStderr(logFile)
+
+			err = cmd.Run()
+
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+
+			Expect(err).To(BeNil())
+
+		})
 	})
 
 })
